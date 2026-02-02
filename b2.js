2019c8bd602a7c0e9458231c94569cdf65559278
NhuQUynh <tentaikhoancuato@gmail.com>
2026-02-02 07:34:30 +0700

    updat

--- b2.js
+++ b2.js
@@ -2,16 +2,22 @@ let sum=0;
 let fizz=0;
 
 for (let i = 1; i <= 50; i++) {
-    if (i%3===0) {
-        console.log("Fizz");
-        fizz++;
-        sum+=i;
+    if (i%5===0 && i%3===0) {
+        console.log("FizzBuzz");
+        
+        
         
     }else if( i%5 === 0){
         console.log("Buzz");
         
-    }else if(i%5===0 && i%3===0){
-        console.log("FizzBuzz");
+    }else if(i%3===0){
+        console.log("Fizz");
+        fizz++;
+        sum+=i;
+        
+        
+    }else{
+        console.log(i);
         
     }
     
